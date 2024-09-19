a1d3227b9826922d926dda03c5c21c5d7dbc8c38
Hao LUO <453972945@qq.com>
2024-09-19 18:20:16 -0400

    Problem one updated

---
+++ 1.Two Sum/Unordered_map_solution.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int>hash_map;
+        int current_num;
+        int complement_num;
+        for (int i = 0; i < nums.size(); i++){
+            current_num = nums[i];
+            complement_num = target - current_num;
+            unordered_map<int, int>::iterator it = hash_map.find(complement_num);
+            if (it == hash_map.end()){
+                hash_map[current_num] = i;
+            } else {
+                return {it->second, i};
+            }
+        }
+        return {-1, -1}; // fail to find the answer
+    }
+};
